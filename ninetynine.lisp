23803d079f2ac6a189736cee06e2fd7dcc6c7079
Alexandru Pana <r1000101@gmail.com>
2012-08-19 22:05:16 +0300

    Switched from tabs to spaces

--- ninetynine.lisp
+++ ninetynine.lisp
@@ -1,8 +1,8 @@
 ;; P05 Reverse a list
 (defun reverse-list (l)
   (if (= (length l) 1)
-	  l
-	(append (reverse-list (rest l)) (list (first l)))))
+      l
+    (append (reverse-list (rest l)) (list (first l)))))
 
 (assert (equal (reverse-list '(1 2 3)) '(3 2 1)))
 (assert (equal (reverse-list '(1)) '(1)))
@@ -20,10 +20,10 @@
 ;; P07 Flatten a nested list structure
 (defun flatten (l)
   (if (= (length l) 0)
-	  '()
-	(if (atom (car l))
-	    (cons (car l) (flatten (cdr l)))
-	  (append (flatten (car l)) (flatten (cdr l))))))
+      '()
+    (if (atom (car l))
+        (cons (car l) (flatten (cdr l)))
+      (append (flatten (car l)) (flatten (cdr l))))))
 
 (assert (equal (flatten '(1 (2 3))) '(1 2 3)))
 (assert (equal (flatten '()) '()))
@@ -32,63 +32,63 @@
 ;; P08 Eliminate consecutive duplicates of list elements
 (defun eliminate-dup (l &optional v)
   (if (= (length l) 0)
-	  l
+      l
     (if (equal (car l) v)
         (eliminate-dup (cdr l) v)
-	  (cons (car l) (eliminate-dup (cdr l) (car l))))))
+      (cons (car l) (eliminate-dup (cdr l) (car l))))))
 
 (assert (equal 
-		  (eliminate-dup '(1 1 1 1 2 2 2 1 1 3 3 3 3 3 3 4 5 6 5 5))
-		  '(1 2 1 3 4 5 6 5)))
+          (eliminate-dup '(1 1 1 1 2 2 2 1 1 3 3 3 3 3 3 4 5 6 5 5))
+          '(1 2 1 3 4 5 6 5)))
 
 ;; P09 Pack consecutive duplicates of list elements into sublists
 (defun pack-consecutive (l &optional v)
   (cond
-	((null l) (list v))
-	((null v) (pack-consecutive (cdr l) (list (car l))))
-	((equal (car v) (car l)) (pack-consecutive (cdr l) (push (car l) v)))
-	(T (cons v (pack-consecutive (cdr l) (list (car l)))))))
+    ((null l) (list v))
+    ((null v) (pack-consecutive (cdr l) (list (car l))))
+    ((equal (car v) (car l)) (pack-consecutive (cdr l) (push (car l) v)))
+    (T (cons v (pack-consecutive (cdr l) (list (car l)))))))
 
 (assert (equal 
-		  (pack-consecutive '(1 1 1 2 2 2 2 3 3 4 5))
-		  '((1 1 1) (2 2 2 2) (3 3) (4) (5))))
+          (pack-consecutive '(1 1 1 2 2 2 2 3 3 4 5))
+          '((1 1 1) (2 2 2 2) (3 3) (4) (5))))
 (assert (equal
-		  (pack-consecutive '(1 2 2 2 2 3 3 3 3 4 4 4 4))
-		  '((1) (2 2 2 2) (3 3 3 3) (4 4 4 4))))
+          (pack-consecutive '(1 2 2 2 2 3 3 3 3 4 4 4 4))
+          '((1) (2 2 2 2) (3 3 3 3) (4 4 4 4))))
 
 ;; P10 Run-length encoding of a list
 (defun rle (l &optional v)
   (cond
-	;; if the list is null, return a list containing v
-	((null l) (list (list (length v) (car v))))
-	;; if v is null, create a new v and call rle recursively
-	((null v) (rle (cdr l) (list (car l))))
-	;; if the first element in l equals the elements in v ...
-	((equal (car v) (car l)) (rle (cdr l) (push (car l) v)))
-	;; return the list formed by v and the recursive call to rle with new v
-	(T (cons (list (length v) (car v)) (rle (cdr l) (list (car l)))))))
+    ;; if the list is null, return a list containing v
+    ((null l) (list (list (length v) (car v))))
+    ;; if v is null, create a new v and call rle recursively
+    ((null v) (rle (cdr l) (list (car l))))
+    ;; if the first element in l equals the elements in v ...
+    ((equal (car v) (car l)) (rle (cdr l) (push (car l) v)))
+    ;; return the list formed by v and the recursive call to rle with new v
+    (T (cons (list (length v) (car v)) (rle (cdr l) (list (car l)))))))
 
 (assert (equal 
-		  (rle '(a a a a b c c a a a d e e e e))
-		  '((4 a) (1 b) (2 c) (3 a) (1 d) (4 e))))
+          (rle '(a a a a b c c a a a d e e e e))
+          '((4 a) (1 b) (2 c) (3 a) (1 d) (4 e))))
 
 ;; P11 Modified run-length encoding (1 B) -> B
 (defun modified-rle (l) (mapcar (lambda (x) (if (= (car x) 1) (car (cdr x)) x)) (rle l)))
 
 (assert (equal 
-		  (modified-rle '(a a a a b c c a a a d e e e e))
-		  '((4 a) b (2 c) (3 a) d (4 e))))
+          (modified-rle '(a a a a b c c a a a d e e e e))
+          '((4 a) b (2 c) (3 a) d (4 e))))
 
 ;; P12 Decode a run-length encoded list
 (defun decode-rle (l)
   (mapcan (lambda (x) 
-			(if (atom x) 
-			    (list x) 
-		      (make-list (first x) :initial-element (second x)))) l))
+            (if (atom x) 
+                (list x) 
+              (make-list (first x) :initial-element (second x)))) l))
 
 (assert (equal 
-		  (decode-rle '((4 a) (2 b) d (3 c)))
-		  '(a a a a b b d c c c)))
+          (decode-rle '((4 a) (2 b) d (3 c)))
+          '(a a a a b b d c c c)))
 
 ;; P14 Duplicate the elements of a list
 (defun dupe (l)
@@ -105,4 +105,9 @@
 (assert (equal (repl '((a b)) 3) '((a b) (a b) (a b))))
 
 ;; P16 Drop every N'th element from a list
+(defun dropnth (l n)
+  (if (> (length l) n)
+      (dropnth (delete-if (lambda (x) t) l :start n :count 1) n)
+    l))
 
+(assert (equal (dropnth '(1 3 5 7 9) 2) '(3 7)))
